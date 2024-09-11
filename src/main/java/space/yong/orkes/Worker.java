f1e879e1afd651e265260b434326e510368ed2d1
Yong Sheng Tan <yong@orkes.io>
2024-09-11 11:02:40 +1000

    Fixes

--- src/main/java/space/yong/orkes/Worker.java
+++ src/main/java/space/yong/orkes/Worker.java
@@ -1,14 +1,33 @@
 package space.yong.orkes;
 
+import com.netflix.conductor.sdk.workflow.executor.task.TaskContext;
 import com.netflix.conductor.sdk.workflow.task.WorkerTask;
-import org.springframework.context.annotation.Lazy;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import java.util.Map;
 
-@Lazy(false)
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class Worker {
-    @WorkerTask(value = "get-ads", threadCount = 5, pollingInterval = 200)
-    public String getAds() {
-        return "hello";
+    public record FxInput(String currencyPair, Double amount) {}
+
+    public Map<String, Double> rates = Map.of(
+        "USDSGD", 1.31,
+        "AUDSGD", 0.87
+    );
+
+    @WorkerTask("calculate-fx")
+    public Double calculateFx(FxInput input) throws Exception {
+        log.info("Received: {} of {}", input.amount(), input.currencyPair());
+        Double rate = rates.get(input.currencyPair());
+        TaskContext.get().addLog("Rate is " + rate);
+        if (rate == null) {
+            var result = TaskContext.get().getTaskResult();
+            result.setOutputData(Map.of("error", "Currency pair not supported"));
+            throw new Exception("Currency pair not supported");
+        }
+        return rate * input.amount();
     }
 }
