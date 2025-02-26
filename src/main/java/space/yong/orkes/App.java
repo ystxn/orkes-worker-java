bfaca6439a1187738b883e39832d9d4534600068
Yong Sheng Tan <yong@orkes.io>
2025-02-26 17:14:37 +0800

    Temp

--- src/main/java/space/yong/orkes/App.java
+++ src/main/java/space/yong/orkes/App.java
@@ -3,7 +3,6 @@ package space.yong.orkes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootApplication(scanBasePackages = {"io.orkes", "space.yong.orkes"})
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
